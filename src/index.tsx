05ec920fe7c296cdee9dd591c262035fef939c9f
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-11-22 15:07:21 +0800

    fix: run

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from './contexts/ConfigContext'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
